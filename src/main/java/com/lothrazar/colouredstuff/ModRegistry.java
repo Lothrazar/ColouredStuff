9db9d2c70a5caf45ea1733a5e794d09a0ca12d89
lothrazar <samson.bassett@gmail.com>
2023-09-08 16:09:50 -0700

    sandstone and other stuff

--- src/main/java/com/lothrazar/colouredstuff/ModRegistry.java
+++ src/main/java/com/lothrazar/colouredstuff/ModRegistry.java
@@ -2,11 +2,12 @@ package com.lothrazar.colouredstuff;
 
 import com.lothrazar.colouredstuff.block.BlockColour;
 import com.lothrazar.library.block.BlockFlib;
+import com.lothrazar.library.item.BlockItemFlib;
+import com.lothrazar.library.item.ItemFlib;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -24,13 +25,15 @@ public class ModRegistry {
 
   public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ModMain.MODID);
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModMain.MODID);
-  public static final RegistryObject<Block> PLANKS_BLOCK = BLOCKS.register("planks", () -> new BlockColour(Block.Properties.copy(Blocks.ACACIA_PLANKS), new BlockFlib.Settings()));
-  public static final RegistryObject<Block> BRICKS_BLOCK = BLOCKS.register("bricks", () -> new BlockColour(Block.Properties.copy(Blocks.STONE_BRICKS), new BlockFlib.Settings()));
-  public static final RegistryObject<Block> SANDSTONE_BLOCK = BLOCKS.register("sandstone", () -> new BlockColour(Block.Properties.copy(Blocks.SANDSTONE), new BlockFlib.Settings()));
+  public static final RegistryObject<Block> PLANKS_BLOCK = BLOCKS.register("planks", () -> new BlockColour(Block.Properties.copy(Blocks.ACACIA_PLANKS).strength(2.0F, 3.0F), new BlockFlib.Settings()));
+  public static final RegistryObject<Block> BRICKS_BLOCK = BLOCKS.register("bricks", () -> new BlockColour(Block.Properties.copy(Blocks.STONE_BRICKS).strength(1.5F, 6.0F), new BlockFlib.Settings()));
+  public static final RegistryObject<Block> SANDSTONE_BLOCK = BLOCKS.register("sandstone", () -> new BlockColour(Block.Properties.copy(Blocks.SANDSTONE).strength(0.8F), new BlockFlib.Settings()));
+  public static final RegistryObject<Block> DIRT_BLOCK = BLOCKS.register("dirt", () -> new BlockColour(Block.Properties.copy(Blocks.DIRT), new BlockFlib.Settings()));
   //  public static final RegistryObject<Block> SANDSTONE_CH_BLOCK = BLOCKS.register("sandstone_chiseled", () -> new BlockColour(Block.Properties.of(), new BlockFlib.Settings()));
-  public static final RegistryObject<Item> PLANKS_ITEM = ITEMS.register("planks", () -> new BlockItem(PLANKS_BLOCK.get(), new Item.Properties()));
-  public static final RegistryObject<Item> BRICKS_ITEM = ITEMS.register("bricks", () -> new BlockItem(BRICKS_BLOCK.get(), new Item.Properties()));
-  public static final RegistryObject<Item> SANDSTONE_ITEM = ITEMS.register("sandstone", () -> new BlockItem(SANDSTONE_BLOCK.get(), new Item.Properties()));
+  public static final RegistryObject<Item> PLANKS_ITEM = ITEMS.register("planks", () -> new BlockItemFlib(PLANKS_BLOCK.get(), new Item.Properties(), new ItemFlib.Settings().tooltip()));
+  public static final RegistryObject<Item> BRICKS_ITEM = ITEMS.register("bricks", () -> new BlockItemFlib(BRICKS_BLOCK.get(), new Item.Properties(), new ItemFlib.Settings().tooltip()));
+  public static final RegistryObject<Item> SANDSTONE_ITEM = ITEMS.register("sandstone", () -> new BlockItemFlib(SANDSTONE_BLOCK.get(), new Item.Properties(), new ItemFlib.Settings().tooltip()));
+  public static final RegistryObject<Item> DIRT_ITEM = ITEMS.register("dirt", () -> new BlockItemFlib(DIRT_BLOCK.get(), new Item.Properties(), new ItemFlib.Settings().tooltip()));
   //  public static final RegistryObject<Item> SANDSTONE_CH_ITEM = ITEMS.register("sandstone_chiseled", () -> new BlockItem(SANDSTONE_CH_BLOCK.get(), new Item.Properties()));
   private static final ResourceKey<CreativeModeTab> TAB = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(ModMain.MODID, "tab"));
 
