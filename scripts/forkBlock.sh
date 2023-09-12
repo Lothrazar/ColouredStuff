1e6d4292db63d45168649ab87e110fb6bfbf9844
lothrazar <samson.bassett@gmail.com>
2023-09-12 06:26:52 -0700

    fix lang auto generat

--- scripts/forkBlock.sh
+++ scripts/forkBlock.sh
@@ -19,7 +19,7 @@ folderdata="src/main/resources/data/${modid}"
 arraylength=${#arr[@]}
 ## now loop through the above array
 # use for loop to read all values and indexes
-echo "\"block.colouredstuff.planks_black\" : \"Black Log\","
+echo "\"block.colouredstuff.${seed}_black\" : \"Black Log\","
 for (( i=0; i<${arraylength}; i++ ));
 do
    color="${arr[$i]}"
@@ -38,10 +38,10 @@ do
 	cp "${folderdata}"/loot_tables/blocks/"${seed}"_black.json "${folderdata}"/loot_tables/blocks/"${seed}"_$color.json
 	sed -i -e "s/black/${color}/g" "${folderdata}"/loot_tables/blocks/"${seed}"_$color.json
    
-#	cp "${folderdata}"/recipes/"${seed}"_black.json "${folderdata}"/recipes/"${seed}"_$color.json
-#	sed -i -e "s/black/${color}/g" "${folderdata}"/recipes/"${seed}"_$color.json
+	cp "${folderdata}"/recipes/"${seed}"_black.json "${folderdata}"/recipes/"${seed}"_$color.json
+	sed -i -e "s/black/${color}/g" "${folderdata}"/recipes/"${seed}"_$color.json
 
-	echo "\"block.colouredstuff.planks_${color}\" : \"${name} Log\","
+	echo "\"block.colouredstuff.${seed}_${color}\" : \"${name} Log\","
 	 
 done
 
